60b6ae08b363f68bf48c8989feff2f3d912d667b
Daffa Hilmy Fadhlurrohman <daffahilmanafrizal@gmail.com>
2025-09-07 14:35:21 +0700

    [ADD]

--- src/core/config.py
+++ src/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Homelab API V1"
+    PROJECT_NAME: str = "Homelab API"
 
 
 settings = Settings()
